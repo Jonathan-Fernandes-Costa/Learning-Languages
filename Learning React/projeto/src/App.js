cc96937800bc3d40cdd800e29747ec41db855a6b
Jonathan Costa <jonathanfernandes638@gmail.com>
2023-02-07 11:57:22 -0300

    Cadastrando colaboradores e elevando pro App.js

--- Learning React/projeto/src/App.js
+++ Learning React/projeto/src/App.js
@@ -1,12 +1,20 @@
+import { useState } from 'react';
 import Banner from './componentes/Banner';
 import Formulario from './componentes/Formulario';
 
 
 function App() {
+
+  const [membros, setMembro] = useState([])
+
+  const newMembro = (membro) =>{
+    console.log(membro)
+    setMembro([...membros], membro)
+  }
   return (
     <div className="App">
       <Banner/>
-      <Formulario/>
+      <Formulario aoMembroCadastrada={membro => newMembro(membro)}/>
     </div>
   );
 }
--- Learning React/projeto/src/componentes/Formulario/index.js
+++ Learning React/projeto/src/componentes/Formulario/index.js
@@ -3,46 +3,56 @@ import Botao from '../Botão'
 import CampoTexto from '../CampoTexto'
 import Lista from '../Lista'
 import './Formulario.css'
-const Formulario = () => {
+const Formulario = (props) => {
     const parentescos = [
-        'Pais', 'Tios', 'Primos', 'Irmãos', 'Avôs', 'Amigos', 'Amigos da Faculdade', 'Outro'
+        'Pai/Mãe', 'Tio(a)', 'Primo(a)', 'Irmão', 'Avó', 'Amigo(a)', 'Amigo da Faculdade', 'Outro'
     ]
     const [nome, setNome] = useState('')//Controlando os estados, o primeiro item da lista
     const [local, setLocal] = useState('')//é responsavel por guardar o valor e o segundo
     const [foto, setFoto] = useState('')//que é uma função, é responsável por atribuir um valor
     const [parentesco, setParentesco] = useState('')
-    
-    const aoSalvar = (evento) =>{
+
+    const aoSalvar = (evento) => {
         evento.preventDefault()
-        console.log("Formulário Submetido => ", nome, local, foto, parentesco)
+        props.aoMembroCadastrada({
+            nome: nome,
+            local: local,
+            foto: foto,
+            parentesco: parentesco
+        })
     }
     return (
         <section className='formulario'>
             <form onSubmit={aoSalvar}>
                 <h2>Preencha os dados para criar o card de familiar</h2>
                 <CampoTexto
-                obrigatorio={true} 
-                label="Nome"
-                placeholder="Digite seu nome" 
-                valor={nome}
-                aoAlterado={valor => setNome(valor) }
+                    obrigatorio={true}
+                    label="Nome"
+                    placeholder="Digite seu nome"
+                    valor={nome}
+                    aoAlterado={valor => setNome(valor)}
                 />
 
-                <CampoTexto 
-                obrigatorio={true} 
-                label="Localização" 
-                placeholder="Digite de onde você é" 
-                valor={local}
-                aoAlterado={valor => setLocal(valor)}
+                <CampoTexto
+                    obrigatorio={true}
+                    label="Localização"
+                    placeholder="Digite de onde você é"
+                    valor={local}
+                    aoAlterado={valor => setLocal(valor)}
                 />
-                <CampoTexto 
-                label="Foto" 
-                placeholder="Digite o endereço da imagem"
-                valor ={foto}
-                aoAlterado = {valor => setFoto(valor)}
+                <CampoTexto
+                    label="Foto"
+                    placeholder="Digite o endereço da imagem"
+                    valor={foto}
+                    aoAlterado={valor => setFoto(valor)}
                 />
-                
-                <Lista  valor={parentesco} aoAlterado={valor => setParentesco(valor)} label='Parentesco' itens={parentescos}/>
+
+                <Lista
+                    label='Parentesco' 
+                    itens={parentescos}
+                    valor={parentesco} 
+                    aoAlterado={valor => setParentesco(valor)} 
+                 />
                 <Botao texto="Criar Card">
                     Criar Card
                 </Botao>
