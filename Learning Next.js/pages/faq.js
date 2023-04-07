e24d6777d5e5dd63dbff069ab3c6016586410a0d
Jonathan Costa <jonathanfernandes638@gmail.com>
2023-04-07 00:14:19 -0300

    Usando Links com o modelo SPA

---
+++ Learning Next.js/pages/faq.js
@@ -0,0 +1,9 @@
+import Link from 'next/link'//Navegação no modelo SPA
+export default function FAQPage(){
+    return (
+        <div>
+            <h1>Teste 2 faq</h1>
+            <Link href="/">teste de link home</Link>
+        </div>
+    )
+}
---
+++ Learning Next.js/pages/index.js
@@ -0,0 +1,10 @@
+import Link from 'next/link'//Usando o modelo SPA
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Teste Ai ze da manga</h1>
+            <Link href="/faq">Ir para o FAQ</Link>
+            
+        </div>
+    )
+}
